995d38343002800c2afd73e92ee96f55359b77f7
Yalung Kiranti <yalungkiranti46@gmail.com>
2024-04-29 22:16:12 +0545

    -created BlogRoles, updated models

--- InterestingBlogWebApp.model/Blogs.cs
+++ InterestingBlogWebApp.model/Blogs.cs
@@ -5,8 +5,13 @@
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-
+        public string ImageURI { get; set; } = string.Empty; 
         public List<Comment> Comments { get; set; } = [];
+        public int Upvotes { get; set; }
+        public int Downvotes { get; set; }
 
+
+        public int UserId { get; set; }
+        public ApplicationUser? User { get; set; }
     }
 }
