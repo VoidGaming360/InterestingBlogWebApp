39dce582ec0e0c0aa158c664fa9fc025462090d1
Yalung Kiranti <yalungkiranti46@gmail.com>
2024-05-01 16:32:13 +0545

    -updated location of the "Interfaces"

---
+++ InterestingBlogWebApp.API/Controllers/BlogsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InterestingBlogWebApp.API.Controllers
+{
+    public class BlogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
