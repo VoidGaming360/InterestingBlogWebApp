dfe1eb427ca0be3c5141192720d6096daee8aff8
Yalung Kiranti <yalungkiranti46@gmail.com>
2024-05-01 16:08:17 +0545

    -example of encapsulation, service injection

--- InterestingBlogWebApp.API/Program.cs
+++ InterestingBlogWebApp.API/Program.cs
@@ -1,4 +1,6 @@
+using InterestingBlogWebApp.Application.Interfaces;
 using InterestingBlogWebApp.Infrastructure.Persistence;
+using InterestingBlogWebApp.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<IBlog, Blog>(); //injecting service
 
 builder.Services.AddDbContext<ApplicationDbContext>();
 
