3a8d1235c35931299b712e028dd625fbbe6ea12c
Yalung Kiranti <yalungkiranti46@gmail.com>
2024-05-01 15:36:34 +0545

    -updated "ApplicationDbContext" and "Program.cs", -added new packages

--- InterestingBlogWebApp.Infrastructure/Persistence/ApplicationDbContext.cs
+++ InterestingBlogWebApp.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace InterestingBlogWebApp.repositories
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InterestingBlogWebApp.Infrastructure.Persistence
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        options.UseSqlite("DataSource = identityDb; Cache=Shared");
     }
 }
