a4b17f48e7c34aaf1037ecb5ecee7718fa6be0ed
Yalung Kiranti <yalungkiranti46@gmail.com>
2024-05-09 13:26:29 +0545

    -update notifcation (mark, seen, unseen, count)

--- InterestingBlogWebApp.API/Controllers/NotificationController.cs
+++ InterestingBlogWebApp.API/Controllers/NotificationController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InterestingBlogWebApp.Application.Common_Interfaces.IServices;
+using InterestingBlogWebApp.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 
 namespace InterestingBlogWebApp.API.Controllers
@@ -6,10 +8,13 @@ namespace InterestingBlogWebApp.API.Controllers
     public class NotificationsController : ControllerBase
     {
         private readonly IHubContext<NotificationHub> _hubContext;
+        private readonly INotificationService _notificationService;
 
-        public NotificationsController(IHubContext<NotificationHub> hubContext)
+        public NotificationsController(IHubContext<NotificationHub> hubContext,
+             INotificationService notificationService)
         {
             _hubContext = hubContext;
+            _notificationService = notificationService;
         }
 
         [HttpPost("send")]
@@ -17,6 +22,22 @@ namespace InterestingBlogWebApp.API.Controllers
         {
             _hubContext.Clients.All.SendAsync("ReceiveNotification", message);
             return Ok(new { Message = "Notification sent successfully!" });
+        }
+
+        [HttpGet("count-unseen")]
+        public async Task<IActionResult> CountUnseenNotifications()
+        {
+            var userId = User.FindFirst("userId")?.Value;
+            var count = await _notificationService.CountUnreadNotifications(userId);
+            return Ok(new { UnseenCount = count });
+        }
+
+        [HttpPost("mark-seen")]
+        public async Task<IActionResult> MarkNotificationsAsSeen()
+        {
+            var userId = User.FindFirst("userId")?.Value;
+            await _notificationService.MarkNotificationsAsRead(userId);
+            return Ok(new { Message = "All notifications marked as seen." });
         }
     }
 }
