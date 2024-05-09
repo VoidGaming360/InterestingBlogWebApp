d0ab9f56ae5e2898173ee99dc75c40344c82278a
Nishant Parajuli <62542121+Bromando64@users.noreply.github.com>
2024-05-09 18:17:12 +0545

    Email Reset Password confirmation

---
+++ InterestingBlogWebApp.Application/DTOs/EmailDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace InterestingBlogWebApp.Application.DTOs
+{
+    public class EmailDTO
+    {
+        public string Email { get; set; }
+    }
+
+    public class PasswordResetDTO
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+    }
+
+}
---
+++ InterestingBlogWebApp.Infrastructure/Services/EmailServices.cs
@@ -0,0 +1,45 @@
+﻿using InterestingBlogWebApp.Application.Common_Interfaces.IServices;
+using MailKit.Net.Smtp;
+using MimeKit;
+using Microsoft.Extensions.Configuration;
+
+namespace InterestingBlogWebApp.Infrastructure.Services
+{
+    public class EmailServices : IEmailServices
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailServices(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string to, string toDisplayName, string subject, string htmlBody)
+        {
+            var emailMessage = new MimeMessage();
+
+            // Fetching 'From' display name and email from configuration
+            string fromDisplayName = _configuration["EmailConfiguration:FromDisplayName"];
+            string fromEmail = _configuration["EmailConfiguration:FromEmail"];
+            emailMessage.From.Add(new MailboxAddress(fromDisplayName, fromEmail));
+
+            // Assuming 'to' includes the full email address and 'toDisplayName' is provided
+            emailMessage.To.Add(new MailboxAddress(toDisplayName, to));
+
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = htmlBody
+            };
+
+            using var client = new SmtpClient();
+            await client.ConnectAsync(_configuration["EmailConfiguration:SetupServer"], int.Parse(_configuration["EmailConfiguration:Port"]), MailKit.Security.SecureSocketOptions.StartTls);
+            await client.AuthenticateAsync(_configuration["EmailConfiguration:Username"], _configuration["EmailConfiguration:Password"]);
+            await client.SendAsync(emailMessage);
+            await client.DisconnectAsync(true);
+        }
+
+
+
+    }
+}
