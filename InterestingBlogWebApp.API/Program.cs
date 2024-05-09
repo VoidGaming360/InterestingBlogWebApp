2f6677359d83cba77166ea101c69c899d409134e
Shisir Basel <110526802+shisirbasel@users.noreply.github.com>
2024-05-09 06:19:08 +0545

    Changes in User Registration

--- InterestingBlogWebApp.API/Program.cs
+++ InterestingBlogWebApp.API/Program.cs
@@ -5,7 +5,28 @@ using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
+<<<<<<< Updated upstream
 builder.Services.AddCustomServices();
+=======
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddCustomServices(builder.Configuration);
+>>>>>>> Stashed changes
+
+const string myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+          policy =>
+          {
+              policy.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+
+          });
+
+});
 
 var app = builder.Build();
 
@@ -19,10 +40,17 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+<<<<<<< Updated upstream
 app.UseAuthentication();
 
 app.UseStaticFiles();
+=======
+
+//app.UseStaticFiles();
+>>>>>>> Stashed changes
 app.UseRouting();
+
+app.UseCors("_myAllowSpecificOrigins");
 
 app.MapControllers();
 
